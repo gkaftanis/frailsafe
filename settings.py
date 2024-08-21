12f375149f5eac65997675ce5139dbb52076b066
gkaftanis <geokas1997@gmail.com>
2024-08-21 19:45:46 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 arango_nodes=['http://localhost:38529']
 arango_db_name='frailSafeProject'
 arango_username='frailsafe_db_user'
-arango_password='frailsafe_gk@1'
+arango_password='frailsafe_db_password'
 
 data_subfolder_list = ['Non-frail' , 'Frail' ,'Pre-frail']
 frail_to_number = {'Frail' : 2 , 'Non-frail' : 0 , 'Pre-frail' : 1}
